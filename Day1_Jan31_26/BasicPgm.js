67fd387aba821d6c183ce7b0dd782240b26a0d76
NM-Pradeep <nmpradeep88@gmail.com>
2026-02-01 07:07:07 +0530

    Add files via upload
    Day1_Jan31_26_Testleaf

---
+++ Day1_Jan31_26/BasicPgm.js
@@ -0,0 +1,20 @@
+//Basic PGM
+
+console.log("Welcome to playwright corse")
+let Duration = 8
+console.log("CourseDuration : "+Duration)
+console.log("Variable type: "+typeof(Duration))
+
+console.log("         ")
+console.log("New Pgm")
+let firstName = "Pradeep";
+let companyName = "Wipro"
+let mobileNumber = 9986799924;
+let isAutoamtion = false;
+let lasPlaywright;
+
+console.log("the firstname is " +firstName, "the variable type is " +typeof(firstName));
+console.log("the companyName is " +companyName, "the variable type is " +typeof(companyName));
+console.log("the mobileNumber is " +mobileNumber, "the variable type is " +typeof(mobileNumber));
+console.log("the isAutoamtion is " +isAutoamtion, "the variable type is " +typeof(isAutoamtion));
+console.log("the lasPlaywright is " +lasPlaywright, "the variable type is " +typeof(lasPlaywright));
